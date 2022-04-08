8076f7be5668a41f534a6b6809751ad61b6f6aab
Felipe Pontin <felipepan64@gmail.com>
2022-04-08 15:34:08 -0300

    Att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var coins = 100000
+var coins = 0
 var perSecond = 0
 
 const valor = {
@@ -21,28 +21,28 @@ const quantidade = {
 
 const update = () => {
     setInterval(() => {
-        document.title = `${coins.toFixed()} Coin Cliker`
+        document.title = `${coins.toFixed(0)} Coin Cliker`
 
-        document.querySelector(".coins").textContent = coins.toFixed()
+        document.querySelector(".coins").textContent = coins.toFixed(0)
         document.querySelector(".perSecond").textContent = perSecond.toFixed(1)
     
-        document.querySelector(".valor__upgrade__portatil").textContent = valor.portatil.toFixed()
-        document.querySelector(".quantidade__portatil").textContent = quantidade.portatil.toFixed()
+        document.querySelector(".valor__upgrade__portatil").textContent = valor.portatil.toFixed(0)
+        document.querySelector(".quantidade__portatil").textContent = quantidade.portatil.toFixed(0)
     
-        document.querySelector(".valor__upgrade__console").textContent = valor.console.toFixed()
-        document.querySelector(".quantidade__console").textContent = quantidade.console.toFixed()
+        document.querySelector(".valor__upgrade__console").textContent = valor.console.toFixed(0)
+        document.querySelector(".quantidade__console").textContent = quantidade.console.toFixed(0)
     
-        document.querySelector(".valor__upgrade__celular").textContent = valor.celular.toFixed()
-        document.querySelector(".quantidade__celular").textContent = quantidade.celular.toFixed()
+        document.querySelector(".valor__upgrade__celular").textContent = valor.celular.toFixed(0)
+        document.querySelector(".quantidade__celular").textContent = quantidade.celular.toFixed(0)
     
-        document.querySelector(".valor__upgrade__notebook").textContent = valor.notebook.toFixed()
-        document.querySelector(".quantidade__notebook").textContent = quantidade.notebook.toFixed()
+        document.querySelector(".valor__upgrade__notebook").textContent = valor.notebook.toFixed(0)
+        document.querySelector(".quantidade__notebook").textContent = quantidade.notebook.toFixed(0)
     
-        document.querySelector(".valor__upgrade__PcGamer").textContent = valor.pcGamer.toFixed()
-        document.querySelector(".quantidade__PcGamer").textContent = quantidade.pcGamer.toFixed()
+        document.querySelector(".valor__upgrade__PcGamer").textContent = valor.pcGamer.toFixed(0)
+        document.querySelector(".quantidade__PcGamer").textContent = quantidade.pcGamer.toFixed(0)
     
-        document.querySelector(".valor__upgrade__bitcoin").textContent = valor.bitcoin.toFixed()
-        document.querySelector(".quantidade__bitcoin").textContent = quantidade.bitcoin.toFixed()
+        document.querySelector(".valor__upgrade__bitcoin").textContent = valor.bitcoin.toFixed(0)
+        document.querySelector(".quantidade__bitcoin").textContent = quantidade.bitcoin.toFixed(0)
     }, 1)
 }
 
@@ -110,5 +110,60 @@ const compraBitcoin = () => {
     }
 }
 
+const saveGame = () => {
+    var gameSave = {
+        coins: coins,
+        perSecond: perSecond,
+        valorPortatil: valor.portatil,
+        quantidadePortatil: quantidade.portatil,
+        valorConsole: valor.console,
+        quantidadeConsole: quantidade.console,
+        valorCelular: valor.celular,
+        quantidadeCelular: quantidade.celular,
+        valorNotebook: valor.notebook,
+        quantidadeNotebook: quantidade.notebook,
+        valorPcGamer: valor.pcGamer,
+        quantidadePcGamer: quantidade.pcGamer,
+        valorBitCoin: valor.bitcoin,
+        quantidadeBitcoin: quantidade.bitcoin
+    }
+    localStorage.setItem("gameSave", JSON.stringify(gameSave))
+}
+
+const loadGame = () => {
+    var savedGame = JSON.parse(localStorage.getItem("gameSave"))
+
+    if (localStorage.getItem("gameSave") !== null) {
+        if (typeof savedGame.perSecond !== "undefined") coins = savedGame.coins
+        if (typeof savedGame.perSecond !== "undefined") perSecond = savedGame.perSecond
+
+        if (typeof savedGame.valorPortatil !== "undefined") valor.portatil = savedGame.valorPortatil
+        if (typeof savedGame.quantidadePortatil !== "undefined") quantidade.portatil = savedGame.quantidadePortatil
+    
+        if (typeof savedGame.valorConsole !== "undefined") valor.console = savedGame.valorConsole
+        if (typeof savedGame.quantidadeConsole !== "undefined") quantidade.console = savedGame.quantidadeConsole
+    
+        if (typeof savedGame.valorCelular !== "undefined") valor.celular = savedGame.valorCelular
+        if (typeof savedGame.quantidadeCelular !== "undefined") quantidade.celular = savedGame.quantidadeCelular
+    
+        if (typeof savedGame.valorNotebook !== "undefined") valor.notebook = savedGame.valorNotebook
+        if (typeof savedGame.quantidadeNotebook !== "undefined") quantidade.notebook = savedGame.quantidadeNotebook
+    
+        if (typeof savedGame.valorPcGamer !== "undefined") valor.pcGamer = savedGame.valorPcGamer
+        if (typeof savedGame.quantidadePcGamer !== "undefined") quantidade.pcGamer = savedGame.quantidadePcGamer
+    
+        if (typeof savedGame.valorBitCoin !== "undefined") valor.bitcoin = savedGame.valorBitCoin
+        if (typeof savedGame.quantidadeBitcoin !== "undefined") quantidade.bitcoin = savedGame.quantidadeBitcoin
+    } 
+}
+
+window.onload = function() {
+    loadGame()
+}
+
 coinsPS()
 update()
+
+setInterval(() => {
+    saveGame()
+}, 1)
